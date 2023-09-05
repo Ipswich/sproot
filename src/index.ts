cb637befc5b000521e51a3cd942ac25c9021a32b
Ben Steele <bsteele146@gmail.com>
2023-09-05 05:18:06 +0100

    Fixin' some let -> const.

--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,14 @@ import { SensorList } from './sensors/SensorList';
 main();
 
 async function main(){
-  let growthDB = new GrowthDB(await mysql2.createConnection({
+  const growthDB = new GrowthDB(await mysql2.createConnection({
     host: process.env['DATABASE_HOST']!,
     user: process.env['DATABASE_USER']!,
     password: process.env['DATABASE_PASSWORD']!,
     database: process.env['DATABASE_NAME']!
   }));
 
-  let sensorList = new SensorList(growthDB);
+  const sensorList = new SensorList(growthDB);
   await sensorList.initialize();
   await sensorList.getReadings();
   await sensorList.addReadingsToDatabase();
