86e3872d3d8ca6e6c3055680bf5d07566d6dcadc
Ipswich <Ipswich@users.noreply.github.com>
2023-10-13 06:19:37 +0000

    Prettier, of course.

--- src/index.ts
+++ src/index.ts
@@ -29,16 +29,16 @@ const swaggerOptions = YAML.load("./openapi.yml");
 swaggerOptions.defaultModelsExpandDepth = -1;
 
 const logger = winston.createLogger({
-  level: 'info',
+  level: "info",
   format: winston.format.json(),
-  defaultMeta: { service: 'user-service' },
+  defaultMeta: { service: "user-service" },
   transports: [
     //
     // - Write all logs with importance level of `error` or less to `error.log`
     // - Write all logs with importance level of `info` or less to `combined.log`
     //
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' }),
+    new winston.transports.File({ filename: "error.log", level: "error" }),
+    new winston.transports.File({ filename: "combined.log" }),
   ],
 });
 
@@ -46,12 +46,13 @@ const logger = winston.createLogger({
 // If we're not in production then log to the `console` with the format:
 // `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
 //
-if (process.env["NODE_ENV"] !== 'production') {
-  logger.add(new winston.transports.Console({
-    format: winston.format.simple(),
-  }));
+if (process.env["NODE_ENV"] !== "production") {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    }),
+  );
 }
-
 
 const app = express();
 
