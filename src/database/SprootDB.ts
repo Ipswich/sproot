cb395636e2020f385a8ea475bee8d0b3a6798a3b
Ipswich <Ipswich@users.noreply.github.com>
2023-10-17 03:42:53 +0000

    Fix a DB thing (maybe).

--- src/database/SprootDB.ts
+++ src/database/SprootDB.ts
@@ -111,7 +111,7 @@ class SprootDB implements ISprootDB {
           readingType,
           sensor.lastReading[readingType as ReadingType],
           sensor.units[readingType as ReadingType],
-          sensor.lastReadingTime?.toISOString(),
+          sensor.lastReadingTime?.toISOString().slice(0, 19).replace('T', ' '),
         ],
       );
     }
