45cffb6364360922c68ab66131dd2c9884f799f2
Ipswich <Ipswich@users.noreply.github.com>
2023-11-12 00:11:44 +0000

    moved some logging around. Can't figure out why the hell this is blocking.

--- server/src/sensors/DS18B20.ts
+++ server/src/sensors/DS18B20.ts
@@ -28,9 +28,12 @@ class DS18B20 extends SensorBase {
   }
 
   override async getReadingAsync(): Promise<void> {
-    const getReadingTimer = this.logger.startTimer();
     try {
+      const getReadingTimer = this.logger.startTimer();
       const result = await util.promisify(ds18b20.temperature)(this.address!);
+      getReadingTimer.done({
+        message: `Reading time for sensor {DS18B20, id: ${this.id}, address: ${this.address}}`,
+      });
       const reading = String(result);
       this.lastReading[ReadingType.temperature] = reading;
       this.lastReadingTime = new Date();
@@ -49,10 +52,6 @@ class DS18B20 extends SensorBase {
       this.logger.error(
         `Failed to get reading for sensor {DS18B20, id: ${this.id}, address: ${this.address}}`,
       );
-    } finally {
-      getReadingTimer.done({
-        message: `Reading time for sensor {DS18B20, id: ${this.id}, address: ${this.address}}`,
-      });
     }
   }
 
