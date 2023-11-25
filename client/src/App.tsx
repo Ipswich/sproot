079027a44cd4a56370bca568411ee6a0c3bdee78
Ipswich <Ipswich@users.noreply.github.com>
2023-11-25 22:23:32 +0000

    Removed dotenv

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,3 @@
-import 'dotenv/config'
 // import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
--- client/src/requests.tsx
+++ client/src/requests.tsx
@@ -1,9 +1,10 @@
-
 import { ISensorBase } from "@sproot/sproot-common/dist/sensors/SensorBase";
 
 async function getSensorData(): Promise<ISensorBase[]> {
-  const response = await fetch(`${process.env["SPROOT_SERVER_URL"]}/api/v1/sensors`);
-  console.log(process.env["SPROOT_SERVER_URL"]);
+  const response = await fetch(
+    `${import.meta.env["REACT_APP_SPROOT_SERVER_URL"]}/api/v1/sensors`,
+  );
+  console.log(import.meta.env["REACT_APP_SPROOT_SERVER_URL"]);
   if (!response.ok) {
     console.error(`Error fetching sensor data: ${response}`);
     return Promise.resolve([]);
