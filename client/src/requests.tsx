756ea34b814f427fee8fd0751418aa0dae7ae204
Ipswich <Ipswich@users.noreply.github.com>
2024-01-04 19:19:33 -0800

    Try this as an arg...

--- client/src/requests.tsx
+++ client/src/requests.tsx
@@ -6,7 +6,7 @@ import {
   ApiSensorsResponse,
 } from "@sproot/src/api/Responses";
 
-const SERVER_URL = `http://${import.meta.env["VITE_HOST"]}`;
+const SERVER_URL = `http://${import.meta.env["VITE_API_URL"]}`;
 
 export async function authenticateAsync(
   username: string,
