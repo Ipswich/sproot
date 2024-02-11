3068ed164e3cded99aa9040869294dcfdfd793ec
Ipswich <Ipswich@users.noreply.github.com>
2024-02-10 22:21:05 -0800

    Update mock menu data.

--- client/src/shell/navbar/NavbarContents.tsx
+++ client/src/shell/navbar/NavbarContents.tsx
@@ -1,12 +1,11 @@
 import { Group, Code, ScrollArea } from '@mantine/core';
 import {
-  IconNotes,
   IconCalendarStats,
   IconGauge,
-  IconPresentationAnalytics,
-  IconFileAnalytics,
   IconAdjustments,
-  IconLock,
+  IconArrowFork,
+  IconBolt,
+  IconTemperatureSun
 } from '@tabler/icons-react';
 
 import { LinksGroup } from './NavbarLinksGroup';
@@ -15,37 +14,16 @@ import classes from './NavbarContents.module.css';
 
 const mockdata = [
   { label: 'Dashboard', icon: IconGauge },
-  {
-    label: 'Market news',
-    icon: IconNotes,
-    initiallyOpened: true,
+  { label: 'Current Conditions', icon: IconTemperatureSun},
+  { label: 'Output States', icon: IconBolt},
+  { label: 'Schedule', icon: IconCalendarStats},
+  { label: 'Triggers', icon: IconArrowFork},
+  { label: 'Settings', icon: IconAdjustments,
     links: [
-      { label: 'Overview', link: '.' },
-      { label: 'Forecasts', link: '.' },
-      { label: 'Outlook', link: '.' },
-      { label: 'Real time', link: '.' },
-    ],
-  },
-  {
-    label: 'Releases',
-    icon: IconCalendarStats,
-    links: [
-      { label: 'Upcoming releases', link: '.' },
-      { label: 'Previous releases', link: '.' },
-      { label: 'Releases schedule', link: '.' },
-    ],
-  },
-  { label: 'Analytics', icon: IconPresentationAnalytics },
-  { label: 'Contracts', icon: IconFileAnalytics },
-  { label: 'Settings', icon: IconAdjustments },
-  {
-    label: 'Security',
-    icon: IconLock,
-    links: [
-      { label: 'Enable 2FA', link: '.' },
-      { label: 'Change password', link: '.' },
-      { label: 'Recovery codes', link: '.' },
-    ],
+      {label: "Sensors", link: "."},
+      {label: "Outputs", link: "."},
+      {label: "System", link: "."}, 
+    ]
   },
 ];
 
@@ -57,6 +35,7 @@ export default function NavbarContents() {
       <div className={classes['header']}>
         <Group justify="space-between">
           {/* <Logo style={{ width: rem(120) }} /> */}
+          <h1>Sproot</h1>
           <Code fw={700}>{import.meta.env["VITE_CLIENT_VERSION"]}</Code>
         </Group>
       </div>
