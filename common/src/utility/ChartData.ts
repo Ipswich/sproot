e658e59de61c96e1113b7881939bd56e0b7f7491
Ipswich <Ipswich@users.noreply.github.com>
2024-07-18 00:15:17 -0700

    These feel safe.

--- common/src/utility/ChartData.ts
+++ common/src/utility/ChartData.ts
@@ -153,7 +153,7 @@ export class ChartData {
     current: Date = new Date(),
   ): boolean {
     const intervalInMs = intervalInMinutes * 60000;
-    let NMinuteDate = new Date(Math.floor(current.getTime() / intervalInMs) * intervalInMs);
+    const NMinuteDate = new Date(Math.floor(current.getTime() / intervalInMs) * intervalInMs);
     if (now.getDate() == NMinuteDate.getDate()) {
       if (now.getHours() == NMinuteDate.getHours()) {
         if (now.getMinutes() == NMinuteDate.getMinutes()) {
@@ -213,7 +213,11 @@ class DataSeriesStats {
         }
         if (!this.counts[key]) {
           this.counts[key] = 0;
+        }
+        if (!this.totals[key]) {
           this.totals[key] = 0;
+        }
+        if (!this.averages[key]) {
           this.averages[key] = 0;
         }
 
