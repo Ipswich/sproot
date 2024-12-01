768a609e4663f9ab915121b69f4e5c5664873312
Ipswich <Ipswich@users.noreply.github.com>
2024-12-01 15:28:22 -0800

    Cleanups

--- server/src/index.ts
+++ server/src/index.ts
@@ -22,7 +22,7 @@ const mysqlConfig = {
   host: process.env["DATABASE_HOST"]!,
   user: process.env["DATABASE_USER"]!,
   password: process.env["DATABASE_PASSWORD"]!,
-  database: Constants.DATABASE_NAME + "-development",
+  database: `${Constants.DATABASE_NAME}-${process.env["NODE_ENV"]}`,
   port: parseInt(process.env["DATABASE_PORT"]!),
   dateStrings: true,
 };
