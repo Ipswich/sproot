6da84482b4fd34d6ee3a23bd935ffff3944b5e20
Ipswich <Ipswich@users.noreply.github.com>
2025-01-11 05:08:45 -0800

    Basic database updates

---
+++ server/src/database/migrations/20250109052057_cameras.ts
@@ -0,0 +1,17 @@
+import type { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+  if (!(await knex.schema.hasTable("cameras"))) {
+    await knex.schema.createTable("cameras", (table) => {
+      table.increments("id").notNullable();
+      table.string("name", 64).notNullable();
+      table.string("deviceIdentifier", 255).notNullable();
+      table.string("resolution", 64).notNullable();
+      table.integer("quality", 11).notNullable();
+    });
+  }
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable("cameras");
+}
