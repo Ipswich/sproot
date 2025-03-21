2a0d2f892c27120cde7e597dda7bd50b6bc7287a
Ipswich <Ipswich@users.noreply.github.com>
2025-03-20 21:09:27 -0700

    Make the Output States table locally orderable
    This is a total convenience factor thing, but having this table
    be locally orderable is a good step in making it easier to handle
    larger lists of outputs. Not an issue if you've only got like 4,
    but it really tied you to the order you created your outputs in.
    
    Now, you can reorder them however you see fit - it's local to
    your device, so it should persist and won't change someone else's
    layout.

---
+++ client/src/routes/output-states/components/SortableAccordionItem.tsx
@@ -0,0 +1,53 @@
+import { useSortable } from "@dnd-kit/sortable";
+import { Accordion, Badge, Group } from "@mantine/core";
+import { IOutputBase } from "@sproot/outputs/IOutputBase";
+import StateCard from "./StateCard";
+import { CSS } from "@dnd-kit/utilities";
+import { IconGripVertical } from "@tabler/icons-react";
+
+interface SortableAccordionItemProps {
+  output: IOutputBase;
+  updateOutputsAsync: () => Promise<void>;
+}
+
+export default function SortableAccordionItem({
+  output,
+  updateOutputsAsync,
+}: SortableAccordionItemProps) {
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    setActivatorNodeRef,
+    transform,
+    transition,
+  } = useSortable({ id: output.id });
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  };
+
+  return (
+    <div ref={setNodeRef} style={style}>
+      <Accordion.Item
+        key={output.id}
+        id={String(output.id)}
+        value={`item-${output.id}`}
+      >
+        <Accordion.Control>
+          <Group>
+            <div ref={setActivatorNodeRef} {...attributes} {...listeners}>
+              <IconGripVertical color={"lightgray"} />
+            </div>
+            <Badge size="xl" radius="sm" color={output.color}>
+              {output.name}
+            </Badge>
+          </Group>
+        </Accordion.Control>
+        <Accordion.Panel>
+          <StateCard output={output} updateOutputsAsync={updateOutputsAsync} />
+        </Accordion.Panel>
+      </Accordion.Item>
+    </div>
+  );
+}
--- client/src/routes/output-states/components/StatesAccordion.tsx
+++ client/src/routes/output-states/components/StatesAccordion.tsx
@@ -1,19 +1,63 @@
+import {
+  DndContext,
+  useSensor,
+  useSensors,
+  PointerSensor,
+  KeyboardSensor,
+  closestCenter,
+  DragEndEvent,
+} from "@dnd-kit/core";
+import {
+  arrayMove,
+  SortableContext,
+  sortableKeyboardCoordinates,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
 import { IOutputBase } from "@sproot/sproot-common/src/outputs/IOutputBase";
-import { Accordion, Badge } from "@mantine/core";
-import StateCard from "./StateCard";
+import { Accordion } from "@mantine/core";
 import { useEffect, useState } from "react";
 import { getOutputsAsync } from "@sproot/sproot-client/src/requests/requests_v2";
 import { useQuery } from "@tanstack/react-query";
+import SortableAccordionItem from "./SortableAccordionItem";
 
 export default function OutputAccordion() {
-  const [outputs, setOutputs] = useState({} as Record<string, IOutputBase>);
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    }),
+  );
+  const [outputs, setOutputs] = useState([] as IOutputBase[]);
   const getOutputsQuery = useQuery({
     queryKey: ["output-states-accordion"],
     queryFn: () => getOutputsAsync(),
   });
 
   const updateOutputsAsync = async () => {
-    setOutputs((await getOutputsQuery.refetch()).data!);
+    const lastOutputStateOrder =
+      localStorage.getItem("outputStateOrder")?.split(",") ?? ([] as string[]);
+    const retrievedOutputs = Object.values(
+      (await getOutputsQuery.refetch()).data!,
+    );
+    const orderedOutputs: IOutputBase[] = [];
+
+    //Get outputs that don't exist in last list
+    const newOutputs = retrievedOutputs.filter(
+      (output) => !lastOutputStateOrder.includes(String(output.id)),
+    );
+
+    //Add the outputs that do exist in the last list
+    lastOutputStateOrder.forEach((outputId) => {
+      const outputIndex = retrievedOutputs.findIndex(
+        (o) => o.id == Number(outputId),
+      );
+      if (outputIndex != -1) {
+        orderedOutputs.push(retrievedOutputs[outputIndex]!);
+      }
+    });
+
+    //Add the new ones to the end of the order
+    setOutputs(orderedOutputs.concat(newOutputs));
   };
 
   useEffect(() => {
@@ -26,31 +70,47 @@ export default function OutputAccordion() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  let value = 0;
-  const items = Object.keys(outputs).map((key) => {
-    value++;
-    const output = outputs[key];
-    if (!output) {
-      return null;
-    }
-
-    return (
-      <Accordion.Item key={output.name} value={`item-${value}`}>
-        <Accordion.Control>
-          <Badge size="xl" radius="sm" color={output.color}>
-            {output.name}
-          </Badge>
-        </Accordion.Control>
-        <Accordion.Panel>
-          <StateCard output={output} updateOutputsAsync={updateOutputsAsync} />
-        </Accordion.Panel>
-      </Accordion.Item>
-    );
-  });
-
+  const items = Object.values(outputs);
+  const sortableItems = items.map((output) => (
+    <SortableAccordionItem
+      output={output}
+      updateOutputsAsync={updateOutputsAsync}
+    />
+  ));
   return getOutputsQuery.isPending ? (
     <p>Loading...</p>
   ) : (
-    <Accordion>{items}</Accordion>
+    <DndContext
+      sensors={sensors}
+      collisionDetection={closestCenter}
+      onDragEnd={handleDragEnd}
+    >
+      <Accordion>
+        <SortableContext items={items} strategy={verticalListSortingStrategy}>
+          {sortableItems}
+        </SortableContext>
+      </Accordion>
+    </DndContext>
   );
+
+  function handleDragEnd(event: DragEndEvent) {
+    const { active, over } = event;
+    if (over == null) {
+      return;
+    }
+
+    if (active.id !== over?.id) {
+      setOutputs((items: IOutputBase[]) => {
+        const oldIndex = items.findIndex((output) => output.id == active.id);
+        const newIndex = items.findIndex((output) => output.id == over!.id);
+
+        const updatedArray = arrayMove(items, oldIndex, newIndex);
+        localStorage.setItem(
+          "outputStateOrder",
+          updatedArray.map((output) => output.id).toString(),
+        );
+        return updatedArray;
+      });
+    }
+  }
 }
