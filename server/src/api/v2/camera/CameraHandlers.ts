f73681891ee72cf89d460ff63ccddce567adbd8c
Ipswich <Ipswich@users.noreply.github.com>
2025-04-20 16:48:27 -0700

    Okay, that didn't help either.

--- server/src/api/v2/camera/CameraHandlers.ts
+++ server/src/api/v2/camera/CameraHandlers.ts
@@ -4,11 +4,7 @@ import { CameraManager } from "../../../camera/CameraManager";
 export async function streamHandlerAsync(request: Request, response: Response): Promise<void> {
   const cameraManger = request.app.get("cameraManager") as CameraManager;
   try {
-    const controller = new AbortController();
-    request.on("close", () => {
-      controller.abort();
-    });
-    await cameraManger.forwardLivestreamAsync(response, controller);
+    await cameraManger.forwardLivestreamAsync(response);
     response.setHeader("Age", 0);
     response.setHeader("Cache-Control", "no-cache, private");
     response.setHeader("Pragma", "no-cache");
