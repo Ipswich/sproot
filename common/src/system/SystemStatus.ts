18aa4463eb8945b11bbf22e5aaf98ce96dbae94d
Ipswich <Ipswich@users.noreply.github.com>
2025-09-30 01:02:12 -0700

    Prettier, of course.

--- common/src/system/SystemStatus.ts
+++ common/src/system/SystemStatus.ts
@@ -2,23 +2,23 @@ export type SystemStatus = {
   system: {
     totalDiskSize: number;
     freeDiskSize: number;
-  },
+  };
   process: {
     uptime: number;
     memoryUsage: number;
     heapUsage: number;
     cpuUsage: number;
-  },
+  };
   database: {
     size: number;
     connectionsUsed: number;
     connectionsFree: number;
     pendingAcquires: number;
     pendingCreates: number;
-  },
+  };
   timelapse: {
     imageCount: number | null;
     directorySize: number | null; // in MB, optional
     lastArchiveGenerationDuration: number | null; // in seconds
-  }
+  };
 };
